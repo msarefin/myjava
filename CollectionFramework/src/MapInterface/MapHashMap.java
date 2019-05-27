d79b2862d09431a6de62d493f4d4e9535b89af55
msarefin <mohammed.arefin@gmail.com>
2019-05-27 10:32:34 -0400

    Update

--- CollectionFramework/src/MapInterface/MapHashMap.java
+++ CollectionFramework/src/MapInterface/MapHashMap.java
@@ -1,20 +1,47 @@
 package MapInterface;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
-
-
+import java.util.Set;
 
 public class MapHashMap {
 
 	public static void main(String[] args) {
-		Map<Integer, String> map = new HashMap<Integer, String>(); 
+
+		Map map = new HashMap();
+
+		map.put(1, "Amit");
+		map.put(2, "Rahul");
+		map.put(3, "Jai");
+		map.put(4, "Amit");
+
+		Set set = map.entrySet();
+
+		Iterator it = set.iterator();
+
+		while (it.hasNext()) {
+			Map.Entry entry = (Map.Entry) it.next();
+
+			System.out.println(entry.getKey() + " | " + entry.getValue());
+
+		}
+
+		System.out.println();
+
+		Map<Integer, String> gMap = new HashMap<Integer, String>();
+
+		gMap.put(101, "Rahman"); 
+		gMap.put(102, "Ali"); 
+		gMap.put(103, "Muhammed"); 
+		gMap.put(104, "Amin");
+		gMap.put(105, "Haidar"); 
 		
-		map.put(100, "Amit"); 
-		map.put(101, "Vijay"); 
-		map.put(102, "Rahul");
+		for(Map.Entry m : gMap.entrySet()) {
+			System.out.println(m.getKey()+" "+m.getValue());
+		}
 		
+	
 		
-		map.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEach(System.out::println);
 	}
 }
