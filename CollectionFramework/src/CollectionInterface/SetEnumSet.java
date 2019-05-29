c22a6f694b6bf6bbfd67a3ec904193982f3185e2
msarefin <mohammed.arefin@gmail.com>
2019-05-29 04:51:35 -0400

    update

--- CollectionFramework/src/CollectionInterface/SetEnumSet.java
+++ CollectionFramework/src/CollectionInterface/SetEnumSet.java
@@ -1,8 +1,28 @@
 package CollectionInterface;
 
+import java.util.EnumSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class SetEnumSet {
 
 	public static void main(String[] args) {
-		
+		Set<days> set = EnumSet.of(days.TUESDAY, days.FRIDAY);
+
+		Set<days> set1 = EnumSet.allOf(days.class);
+		Set<days> set2 = EnumSet.noneOf(days.class);
+
+		Iterator<days> iter = set.iterator();
+		while (iter.hasNext()) {
+			System.out.println(iter.next());
+		}
+
+		System.out.println(set1);
+		System.out.println(set2);
+
 	}
+}
+
+enum days {
+	SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
 }
