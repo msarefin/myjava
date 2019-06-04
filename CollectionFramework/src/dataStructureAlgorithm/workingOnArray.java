eaa8353176459f747ac84756bd3aafe891599316
msarefin <mohammed.arefin@gmail.com>
2019-06-04 06:09:27 -0400

    update

--- CollectionFramework/src/dataStructureAlgorithm/workingOnArray.java
+++ CollectionFramework/src/dataStructureAlgorithm/workingOnArray.java
@@ -62,4 +62,31 @@ public class workingOnArray {
 
 	}
 
+	int a[] = {};
+	int nElems;
+	int searchKey;
+
+	public int find(long search) {
+		int lowerBound = 0;
+		int upperBound = nElems - 1;
+
+		int curIn;
+
+		while (true) {
+			curIn = (lowerBound + upperBound) / 2;
+			if (a[curIn] == searchKey)
+				return curIn;
+			else if (lowerBound > upperBound)
+				return nElems;
+			else {
+				if (a[curIn] < searchKey)
+					lowerBound = curIn + 1;
+				else
+					upperBound = curIn - 1;
+
+			}
+
+		}
+	}
+
 }
