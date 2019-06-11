31db3a74c6a3cc2e01e8085f00b286a8aed710fe
msarefin <mohammed.arefin@gmail.com>
2019-06-11 05:04:36 -0400

    update

--- CollectionFramework/src/practice/Practice.java
+++ CollectionFramework/src/practice/Practice.java
@@ -1,5 +1,6 @@
 package practice;
 
+import java.lang.reflect.Array;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -19,13 +20,11 @@ public class Practice {
 //			Map.Entry entry = (Map.Entry) it.next();
 //			System.out.println(entry.getKey() + " " + entry.getValue());
 //		}
-		
-		
-		int maxSize = 100; 
-		classDataArray arr; 
-		arr = new classDataArray(maxSize); 
-		
-		
+
+		int maxSize = 100;
+		classDataArray arr;
+		arr = new classDataArray(maxSize);
+
 		arr.insert("Evans", "Patty", 24);
 		arr.insert("Smith", "Lorriane", 37);
 		arr.insert("Yee", "Tom", 43);
@@ -36,30 +35,54 @@ public class Practice {
 		arr.insert("Lamarque", "Henry", 54);
 		arr.insert("vang", "Minh", 22);
 		arr.insert("Careswell", "Lucinda", 18);
-		
+
+		arr.displayA();
+
+		String searchKey = "Stimson";
+		person found;
+
+		found = arr.find(searchKey);
+
+		if (found != null) {
+			System.out.print("Found ");
+			found.displayPerson();
+		} else
+			System.out.println("Cant find " + searchKey);
+
+		System.out.println("Deleting Smith, Yee  and Careswell");
+
+		arr.delete("Smith");
+		arr.delete("Yee");
+		arr.delete("Careswell");
+
 		arr.displayA();
 		
-		String searchKey = "Stimson"; 
-		person found; 
+		System.out.println("objetct sorting");
+		 
+		ArrayInObj arro; 
+		arro = new ArrayInObj(maxSize); 
 		
-		found = arr.find(searchKey);
 		
-	if(found !=null) {
-		System.out.print("Found ");
-		found.displayPerson();
-	}
-	else 
-		System.out.println("Cant find "+ searchKey); 
-	
-	System.out.println("Deleting Smith, Yee  and Careswell");
-	
-	arr.delete("Smith");
-	arr.delete("Yee");
-	arr.delete("Careswell");
-	
-	
-	arr.displayA();
+		arro.insert("Evans", "Patty", 24);
+		arro.insert("Smith", "Doc", 59);
+		arro.insert("Smith", "Lorraine", 37);
+		arro.insert("Smith", "Paul", 37);
+		arro.insert("Yee","Tom", 43);
+		arro.insert("Hashimoto", "Sato", 21);
+		arro.insert("Stimson", "Henry", 29);
+		arro.insert("Velasquez", "Jose", 72);
+		arro.insert("Vang","Minh", 22);
+		arro.insert("Creswell", "Lucinda", 18);
 
+		System.out.println("befor sorting");
+		
+		arro.display();
+		arro.insertionSort();
+		
+		System.out.println("after sorting");
+		arro.display();
+		
+		
 	}
 }
 
@@ -82,6 +105,49 @@ class person {
 
 	public String getlast() {
 		return lastname;
+	}
+
+}
+
+class ArrayInObj {
+	private person[] a;
+	private int nElems;
+
+	public ArrayInObj(int max) {
+		a = new person[max];
+		nElems = 0;
+
+	}
+
+	public void insert(String First, String Last, int age) {
+		a[nElems] = new person(Last, First, age);
+		nElems++;
+	}
+
+	public void display() {
+		for (int i = 0; i < nElems; i++) {
+			a[i].displayPerson();
+			
+		}
+
+		System.out.println(" ");
+	}
+
+	public void insertionSort() {
+
+		int in, out;
+
+		for (out = 1; out < nElems; out++) {
+			person temp = a[out];
+			in = out;
+
+			while (in > 0 && a[in - 1].getlast().compareTo(temp.getlast()) > 0) {
+				a[in] = a[in - 1];
+				--in;
+			}
+
+			a[in] = temp;
+		}
 	}
 
 }
--- CollectionFramework/src/stackQMap/StackQMap.java
+++ CollectionFramework/src/stackQMap/StackQMap.java
@@ -3,10 +3,54 @@ package stackQMap;
 public class StackQMap {
 
 	public static void main(String[] args) {
-		
+
+		stackX s = new stackX(10);
+
+		s.push(20);
+		s.push(40);
+		s.push(60);
+		s.push(80);
+
+		while (!s.isEmpty()) {
+			int value = s.pop();
+			System.out.print(value + " ");
+		}
+
+		System.out.println();
 	}
 }
 
-class stack{
-	
+class stackX {
+
+	private int size, top;
+	private int[] arr;
+
+	public stackX(int s) {
+		size = s;
+		arr = new int[s];
+		top = -1;
+	}
+
+	public void push(int j) {
+		arr[++top] = j;
+	}
+
+	public int pop() {
+		return arr[top--];
+
+	}
+
+	public int peek() {
+		return arr[top];
+
+	}
+
+	public boolean isEmpty() {
+		return (top == -1);
+	}
+
+	public boolean isFull() {
+		return (top == size - 1);
+	}
+
 }
