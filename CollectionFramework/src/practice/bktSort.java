0137285e2e4ca0669771ca93bf7c1e41801d3ca2
msarefin <mohammed.arefin@gmail.com>
2019-06-19 13:48:46 -0400

    update

---
+++ CollectionFramework/src/practice/bktSort.java
@@ -0,0 +1,102 @@
+package practice;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class bktSort {
+
+	static int range = 100; // setting the range
+	static int size = range / 10;
+	static int[] ar = new int[size]; // setting the size of the array
+
+	public static void main(String[] args) {
+
+//		Filling the array with random numbers within range
+
+		for (int i = 0; i < ar.length; i++) {
+			ar[i] = new Random().nextInt(range);
+		}
+
+		for (int array : ar) {
+			System.out.print(array + " ");
+		}
+		System.out.println("\n");
+//		Finding the largest number within the array
+
+		int temp = 0;
+
+		for (int i = 0; i < ar.length; i++) {
+			if (temp < ar[i]) {
+				temp = ar[i];
+
+			}
+		}
+
+		int ind = size / 3; // Setting the number of the buckets
+		Buckets[] buckets = new Buckets[ind]; // creating an array of buckets
+
+		for (int i = 0; i < buckets.length; i++) {
+			buckets[i] = new Buckets(); // initializing using object of buckets
+		}
+
+		for (int a : ar) {
+			int bi = a * ind / (temp + 1); // Creating a bucket index number
+			buckets[bi].bkt.add(a);
+
+		}
+
+		for (Buckets n : buckets) {
+			System.out.print("Bucket ");
+			for (int val : n.bkt) {
+				System.out.print(val + " ");
+			}
+			System.out.println();
+		}
+
+		// Sorting suing insertion sort
+
+		System.out.println();
+
+		for (Buckets b : buckets) {
+			for (int i = 0; i < b.bkt.size() - 1; i++) {
+				int p = i;
+				while (p >= 0 && b.bkt.get(p) > b.bkt.get(p + 1)) {
+					temp = b.bkt.get(p);
+					b.bkt.set(p, b.bkt.get(p + 1));
+					b.bkt.set(p + 1, temp);
+
+					p--;
+
+				}
+
+			}
+
+		}
+
+		System.out.println("--------------------------- \n");
+		int index = 0;
+		for (Buckets n : buckets) {
+			System.out.print("Bucket ");
+			for (int val : n.bkt) {
+				System.out.print(val + " ");
+
+			}
+			System.out.println();
+			
+			for(int val : n.bkt) {
+				ar[index] =  val;
+				index++; 
+			}
+		}
+		System.out.println();
+		for(int num : ar) {
+			System.out.print(num+" ");
+		}
+
+	}
+
+}
+
+class Buckets {
+	ArrayList<Integer> bkt = new ArrayList<Integer>();
+}
--- CollectionFramework/src/sortingAlgorithms/BucketSort.java
+++ CollectionFramework/src/sortingAlgorithms/BucketSort.java
@@ -1,5 +1,53 @@
 package sortingAlgorithms;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class BucketSort {
 
+	private static int arr[];
+	private static int size = 10;
+	private static int bound = 100;
+
+	public static void main(String[] args) {
+		arr = initializeArray(arr, size, bound);
+		displayArray(arr);
+		sort(arr, size);
+		displayArray(arr);
+	}
+
+	private static int[] initializeArray(int arr[], int size, int bound) {
+		arr = new int[size];
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = new Random().nextInt(bound);
+		}
+		return arr;
+	}
+
+	private static void displayArray(int arr[]) {
+		System.out.println(Arrays.toString(arr));
+	}
+
+	private static void sort(int arr[], int size) {
+		int bsize = (int)Math.sqrt(size); 
+		int bucket[] = new int[bsize];
+		
+		for(int i = 0 ; i<bucket.length; i++) {
+			bucket[i] = 0; 
+		}
+		
+		for(int i =0 ; i<arr.length; i++) {
+			bucket[arr[i]]++; 
+		}
+		
+		int outPos= 0; 
+		for(int i = 0; i<bucket.length; i++) {
+			for(int j = 0; j<bucket[i]; j++) {
+				arr[outPos++] = i; 
+			}
+		}
+		
+		
+	}
 }
