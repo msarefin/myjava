b80ed54e97c151a2eb6cb06172ffa5a26a3860a3
msarefin <mohammed.arefin@gmail.com>
2019-07-03 16:11:35 -0400

    update

---
+++ InputOutputExternalData/src/accessingMongoDB/MongoOnline.java
@@ -0,0 +1,52 @@
+package accessingMongoDB;
+
+import java.util.Iterator;
+
+import javax.print.Doc;
+
+import org.bson.Document;
+
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoCredential;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoOnline {
+
+	public static void main(String[] args) {
+		String link = "mongodb+srv://arefin:qatester@cluster0-bqtnc.mongodb.net/test?retryWrites=true&w=majority";
+
+		MongoClientURI uri = new MongoClientURI(link);
+
+		MongoClient mongoClient = new MongoClient(uri);
+		
+//		MongoCredential credential = MongoCredential.createCredential("arefin", "pnt", "qatester".toCharArray());
+		
+		MongoDatabase database = mongoClient.getDatabase("pnt");
+		
+		try {
+			
+			database.createCollection("Tutorial");
+			System.out.println("collection created");
+			
+		}catch (Exception e) {
+			System.out.println("collection already exits");
+		}
+		
+		MongoCollection<Document> collection  = database.getCollection("Tutorial");
+		System.out.println("Collection selection");
+		
+		
+		Document document  = new Document("Title","QA Tester")
+				.append("Name", "Daniel")
+				.append("Description", "Web Applipcation Tester")
+				.append("company", "amazon.com")
+				.append("Location", "Boston, MA");
+		
+		collection.insertOne(document);
+
+	}
+}
--- InputOutputExternalData/src/accessingMongoDB/WritingToMongodb.java
+++ InputOutputExternalData/src/accessingMongoDB/WritingToMongodb.java
@@ -1,32 +1,82 @@
 package accessingMongoDB;
 
+import java.util.Iterator;
+
 import org.bson.Document;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;;
 
 public class WritingToMongodb {
 
 	public static void main(String[] args) {
 
-		MongoClient mongo = new MongoClient("localhost", 27017);
-
-		MongoCredential credencial;
-		credencial = MongoCredential.createCredential("arefin", "pnt", "abc123".toCharArray());
-
-		MongoDatabase database = mongo.getDatabase("pnt");
+//		create mongoClient
+		MongoClient mongo = new MongoClient("localhost",27017);
+		System.out.println("MongoClient Created");
 		
+		String bdName = "PTT1";
+		
+//		create credenctial
+		MongoCredential credencial = MongoCredential.createCredential("arefin", bdName, "abc123".toCharArray());
+		System.out.println("mongoCredencial Created");
+		
+		//accessing database
+		MongoDatabase database = mongo.getDatabase(bdName);
+		System.out.println("database accessed");
+//		System.out.println("Credencials: "+credencial);
+		
+		
+		
+//		creating collection 
 		try {
-			database.createCollection("SimpleCollection");
+			database.createCollection("MySampleCollection");
+			System.out.println("new collection added");
+			
 		}catch (Exception e) {
-			System.err.println("Collection already exist !!");
+			// TODO: handle exception
+			System.out.println("collection already exit!!");
 		}
 		
-		MongoCollection<Document> collection = database.getCollection("SimpleCollection");
-
-		Document document = new Document("title");
+//		retreiving collection 
+		MongoCollection<Document> collection = database.getCollection("MySampleCollection");
+		System.out.println("collection selected");
 		
+//		adding dcoument to collection
+		Document document = new Document("title","QA Tester")
+				.append("id", 1).append("name", "Mohammed")
+				.append("description", "Web Automation")
+				.append("company", "amazon.com")
+				.append("location", "California, USA");
+		
+		collection.insertOne(document);
+		
+		collection.updateOne(Filters.eq("id",1), Updates.set("name", "Danny"));
+		
+		collection.deleteMany(Filters.all("name", "Mohammed"));
+		
+		
+		collection = database.getCollection("MySampleCollection");
+		System.out.println("collection selected");
+		
+//		get iterable object 
+		
+		FindIterable<Document> iterDoc = collection.find(); 
+		
+		
+		int i = 1; 
+		
+		Iterator it = iterDoc.iterator(); 
+		
+		while(it.hasNext()) {
+			System.out.println(it.next());
+			i++; 
+		}
+
 	}
 }
