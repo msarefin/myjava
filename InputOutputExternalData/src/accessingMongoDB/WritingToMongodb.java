c6d9f72d3cb8e993048063ffe1f99e30abe05a74
msarefin <mohammed.arefin@gmail.com>
2019-07-02 20:52:14 -0400

    update

--- InputOutputExternalData/src/accessingMongoDB/WritingToMongodb.java
+++ InputOutputExternalData/src/accessingMongoDB/WritingToMongodb.java
@@ -1,5 +1,32 @@
 package accessingMongoDB;
 
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;;
+
 public class WritingToMongodb {
 
+	public static void main(String[] args) {
+
+		MongoClient mongo = new MongoClient("localhost", 27017);
+
+		MongoCredential credencial;
+		credencial = MongoCredential.createCredential("arefin", "pnt", "abc123".toCharArray());
+
+		MongoDatabase database = mongo.getDatabase("pnt");
+		
+		try {
+			database.createCollection("SimpleCollection");
+		}catch (Exception e) {
+			System.err.println("Collection already exist !!");
+		}
+		
+		MongoCollection<Document> collection = database.getCollection("SimpleCollection");
+
+		Document document = new Document("title");
+		
+	}
 }
