e854970b9f9210fb44c4b9a4f9a3b57d27404c12
msarefin <mohammed.arefin@gmail.com>
2019-07-27 23:23:56 -0400

    update

--- WebAutomation/src/main/java/Practice/WaitToLoad.java
+++ WebAutomation/src/main/java/Practice/WaitToLoad.java
@@ -2,7 +2,6 @@ package Practice;
 
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,6 +12,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.google.common.base.Function;
 
 public class WaitToLoad {
 
@@ -39,21 +40,31 @@ public class WaitToLoad {
 
 		WebDriverWait wdWait = new WebDriverWait(driver, 10);
 
+//		Explicitely Wait
 		wdWait.until(
 				ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".tp-mask-wrap>#slide-6-layer-1>img")));
-		System.out.println(driver.getTitle());
+	
 
-		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
-			    .withTimeout(30, TimeUnit.SECONDS)
-			    .pollingEvery(5, TimeUnit.SECONDS)
-			    .ignoring(NoSuchElementException.class);
+//		FluentWait
+		
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(10, TimeUnit.SECONDS).pollingEvery(1,TimeUnit.SECONDS).ignoring(NoSuchElementException.class);
+		
+		
 
-			WebElement foo = wait.until(new Function<WebDriver, WebElement>() 
-			{
-			  public WebElement apply(WebDriver driver) {
-			  return driver.findElement(By.id(".tp-mask-wrap>#slide-6-layer-1>img"));
-			}
-			});
+//		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(10, TimeUnit.SECONDS)
+//				.pollingEvery(2, TimeUnit.SECONDS).ignoring(NoSuchElementException.class);
+//		
+//		WebElement element = wait.until(new Function<WebDriver, WebElement>() {
+//			public WebElement apply(WebDriver driver) {
+//				return driver.findElement(By.cssSelector("Css"));
+//			}
+//		}); 
+		
+		wait.until(ExpectedConditions.alertIsPresent());
+
+		driver.manage().timeouts().pageLoadTimeout(100, TimeUnit.SECONDS);
+		
+		driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS); 
 		
 		driver.close();
 	}
