a75fbb6dc4ea9cb60fbcfd27346eacb31af6070d
msarefin <mohammed.arefin@gmail.com>
2019-07-28 01:10:53 -0400

    update

--- WebAutomation/src/main/java/Practice/WaitToLoad.java
+++ WebAutomation/src/main/java/Practice/WaitToLoad.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Ignore;
 
 import com.google.common.base.Function;
 
@@ -43,14 +44,17 @@ public class WaitToLoad {
 //		Explicitely Wait
 		wdWait.until(
 				ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".tp-mask-wrap>#slide-6-layer-1>img")));
-	
 
 //		FluentWait
-		
-		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(10, TimeUnit.SECONDS).pollingEvery(1,TimeUnit.SECONDS).ignoring(NoSuchElementException.class);
-		
-		
 
+	Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(10,TimeUnit.SECONDS).pollingEvery(1, TimeUnit.SECONDS).ignoring(NoSuchElementException.class);
+
+	WebElement element = wait.until(new Function<WebDriver, WebElement>() {
+		public WebElement apply(WebDriver driver) {
+			return driver.findElement(By.cssSelector("css"));
+		}
+	}); 
+		
 //		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(10, TimeUnit.SECONDS)
 //				.pollingEvery(2, TimeUnit.SECONDS).ignoring(NoSuchElementException.class);
 //		
@@ -59,13 +63,13 @@ public class WaitToLoad {
 //				return driver.findElement(By.cssSelector("Css"));
 //			}
 //		}); 
-		
-		wait.until(ExpectedConditions.alertIsPresent());
+
+//		wait.until(ExpectedConditions.alertIsPresent());
 
 		driver.manage().timeouts().pageLoadTimeout(100, TimeUnit.SECONDS);
-		
-		driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS); 
-		
+
+		driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+
 		driver.close();
 	}
 
