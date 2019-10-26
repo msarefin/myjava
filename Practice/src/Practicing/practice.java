fe999eb37766fed035c160f3c243a6b2c67dc3e3
msarefin <mohammed.arefin@gmail.com>
2019-10-26 00:21:43 -0400

    updated

--- Practice/src/Practicing/practice.java
+++ Practice/src/Practicing/practice.java
@@ -1,5 +1,8 @@
 package Practicing;
 
+
 public class practice {
 
+	
+	
 }
---
+++ WebAutomation/src/main/java/practicting/Practice.java
@@ -0,0 +1,12 @@
+package practicting;
+
+import org.testng.annotations.BeforeMethod;
+
+public class Practice {
+
+	@BeforeMethod
+	public void setup() {
+		
+	}
+	
+}
