114e5044e32df823c1662205fc8bf67b4a44310e
msarefin <mohammed.arefin@gmail.com>
2019-12-14 00:30:16 -0500

    update

--- Practice/src/practice/GraphImplementation.java
+++ Practice/src/practice/GraphImplementation.java
@@ -5,68 +5,47 @@ import java.util.LinkedList;
 
 public class GraphImplementation {
 
-	private int V;
-	private LinkedList<Integer> adj[];
+	 int V;
+	 LinkedList<Integer> adjListArray[];
 
-	public GraphImplementation(int v) {
+	 void Graph(int v) {
 		this.V = v;
-		adj = new LinkedList[v];
-		for (int i = 0; i < v; i++) {
-			adj[i] = new LinkedList<>();
+		adjListArray = new LinkedList[V]; // Define the size of the array - the number of vertices
+		
+		for(int i = 0; i<v; i++) {
+			adjListArray[i] = new LinkedList<>(); // Initialize the with a list of vertices
 		}
 	}
-
-	void addEdge(int v, int w) {
-		adj[v].add(w);
-		adj[w].add(v);
+	 
+ void addEdge(GraphImplementation g, int src, int dst) { // adding edge to a undirectional graph
+		g.adjListArray[src].add(dst); // adding edge from source to destination
+		g.adjListArray[dst].add(src);// adding edge from destination to source
 	}
-
-	void printVertexCover() {
-		boolean visited[] = new boolean[this.V];
-		for (int i = 0; i < this.V; i++) {
-			visited[i] = false;
-
-		}
-
-		Iterator<Integer> i;
-
-		for (int u = 0; u < V; u++) {
-			// An edge is only picked when both visited[u]
-			// and visited[v] are false
-			if (visited[u] == false) {
-				// Go through all adjacents of u and pick the
-				// first not yet visited vertex (We are basically
-				// picking an edge (u, v) from remaining edges.
-				i = adj[u].iterator();
-				while (i.hasNext()) {
-					int v = i.next();
-					if (visited[v] == false) {
-						// Add the vertices (u, v) to the result
-						// set. We make the vertex u and v visited
-						// so that all edges from/to them would
-						// be ignored
-						visited[v] = true;
-						visited[u] = true;
-						break;
-					}
-				}
+	
+	 void printGraph(GraphImplementation g) {
+		for(int v = 0; v<g.V; v++) {
+			System.out.print("head");
+			for(Integer n : g.adjListArray[v]) {
+				System.out.print("->"+n);
 			}
+			System.out.println();
 		}
-		for (int j=0; j<V; j++) 
-            if (visited[j]) 
-              System.out.print(j+" "); 
 	}
 	
 	public static void main(String[] args) {
-		GraphImplementation g = new GraphImplementation(7); 
-        g.addEdge(0, 1); 
-        g.addEdge(0, 2); 
-        g.addEdge(1, 3); 
-        g.addEdge(3, 4); 
-        g.addEdge(4, 5); 
-        g.addEdge(5, 6); 
-  
-        g.printVertexCover(); 
+		GraphImplementation g = new GraphImplementation(); 
+		
+		g.Graph(5);
+		
+		g.addEdge(g, 0, 1);
+		g.addEdge(g, 0, 4);
+		g.addEdge(g, 1, 2);
+		g.addEdge(g, 1, 3);
+		g.addEdge(g, 1, 4);
+		g.addEdge(g, 2, 3);
+		g.addEdge(g, 3, 4);
+		
+		
+		g.printGraph(g);
 	}
-
 }
