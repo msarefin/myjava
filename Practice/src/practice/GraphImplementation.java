a3502c3175737d1b66ea5630172e0a62ca077efa
msarefin <mohammed.arefin@gmail.com>
2019-12-15 12:04:57 -0500

    update

--- Practice/src/practice/GraphImplementation.java
+++ Practice/src/practice/GraphImplementation.java
@@ -5,38 +5,38 @@ import java.util.LinkedList;
 
 public class GraphImplementation {
 
-	 int V;
-	 LinkedList<Integer> adjListArray[];
+	int V;
+	LinkedList<Integer> adjListArray[];
 
-	 void Graph(int v) {
+	void Graph(int v) {
 		this.V = v;
 		adjListArray = new LinkedList[V]; // Define the size of the array - the number of vertices
-		
-		for(int i = 0; i<v; i++) {
+
+		for (int i = 0; i < v; i++) {
 			adjListArray[i] = new LinkedList<>(); // Initialize the with a list of vertices
 		}
 	}
-	 
- void addEdge(GraphImplementation g, int src, int dst) { // adding edge to a undirectional graph
+
+	void addEdge(GraphImplementation g, int src, int dst) { // adding edge to a undirectional graph
 		g.adjListArray[src].add(dst); // adding edge from source to destination
 		g.adjListArray[dst].add(src);// adding edge from destination to source
 	}
-	
-	 void printGraph(GraphImplementation g) {
-		for(int v = 0; v<g.V; v++) {
-			System.out.print("head : "+v );
-			for(Integer n : g.adjListArray[v]) {
-				System.out.print("->"+n);
+
+	void printGraph(GraphImplementation g) {
+		for (int v = 0; v < g.V; v++) {
+			System.out.print("head : " + v);
+			for (Integer n : g.adjListArray[v]) {
+				System.out.print("->" + n);
 			}
 			System.out.println();
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		GraphImplementation g = new GraphImplementation(); 
-		
+		GraphImplementation g = new GraphImplementation();
+
 		g.Graph(5);
-		
+
 		g.addEdge(g, 0, 1);
 		g.addEdge(g, 0, 4);
 		g.addEdge(g, 1, 2);
@@ -44,8 +44,7 @@ public class GraphImplementation {
 		g.addEdge(g, 1, 4);
 		g.addEdge(g, 2, 3);
 		g.addEdge(g, 3, 4);
-		
-		
+
 		g.printGraph(g);
 	}
 }
