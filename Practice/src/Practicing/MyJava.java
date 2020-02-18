d00e5692ed0985617ab909a5aed86b642bee8746
msarefin <mohammed.arefin@gmail.com>
2020-02-18 10:21:18 -0500

    update

--- Practice/src/Practicing/MyJava.java
+++ Practice/src/Practicing/MyJava.java
@@ -1,8 +1,6 @@
 package Practicing;
 
 import java.util.List;
-import java.beans.beancontext.BeanContext;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
