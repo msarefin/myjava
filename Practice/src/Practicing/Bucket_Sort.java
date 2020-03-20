66a774362bf974b872c6ace8ae37c3de3ceb4a61
msarefin <mohammed.arefin@gmail.com>
2020-03-19 22:22:29 -0400

    bucketSort

--- Practice/src/Practicing/Bucket_Sort.java
+++ Practice/src/Practicing/Bucket_Sort.java
@@ -1,8 +1,8 @@
 package Practicing;
 
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Bucket_Sort {
 
@@ -10,103 +10,92 @@ public class Bucket_Sort {
 
 	public static void main(String[] args) {
 		bucketSort(arr);
-
+		System.out.println("Sorted Array:");
+		printArray(arr);
 	}
 
 	private static void bucketSort(int[] arr) {
-		String method = new Throwable().getStackTrace()[0].getMethodName().toString();
-		System.out.println(method + "\n" + "=".repeat(method.length()));
+		String methodName = new Throwable().getStackTrace()[0].getMethodName();
+		System.out.println(methodName + "\n" + "=".repeat(methodName.length()));
 		printArray(arr);
 
-		int max = 0;
 		int min = 0;
-
-		int nas = 0;
-		int pas = 0;
+		int max = 0;
+		int ne = 0;
+		int pe = 0;
 
 		for (int n : arr) {
 			if (n < 0) {
-				nas++;
-				if (min > n)
+				ne++;
+				if (min > n) {
 					min = n;
+				}
 			} else {
-				pas++;
-				if (max < n)
+				pe++;
+				if (max < n) {
 					max = n;
+				}
 			}
 		}
 
-		int nbs = (int) Math.sqrt(nas);
-		int pbs = (int) Math.sqrt(pas);
+		int nb = (int) Math.sqrt(ne);
+		int pb = (int) Math.sqrt(pe);
 
-//		System.out.println("max\t" + max + "\nmin\t" + min + "\nnas\t" + nas + "\npas\t" + pas + "\nnbs\t" + nbs
-//				+ "\npbs\t" + pbs);
+		List[] nBucket = new List[nb];
+		List[] pBucket = new List[pb];
 
-		List[] nBucket = new List[nbs];
-		List[] pBucket = new List[pbs];
-
-		for (int i = 0; i < nBucket.length; i++) {
-			nBucket[i] = new ArrayList();
-		}
-		for (int i = 0; i < pBucket.length; i++) {
-			pBucket[i] = new ArrayList();
-		}
-
-		for (int n : arr) {
-			int bi = 0;
-
-			if (n < 0) {
-				bi = (n * nbs) / (min - 1);
+		nBucket = initializeBucket(nBucket);
+		pBucket = initializeBucket(pBucket);
+		
+		for(int n :arr) {
+			int bi = 0; 
+			if(n<0) {
+				bi = (n*nb)/(min-1);
 				nBucket[bi].add(n);
-
-			} else {
-				bi = (n * pbs) / (max + 1);
-				pBucket[bi].add(n);
+			}else {
+				bi = (n*pb)/(max+1);
+				pBucket[bi].add(n); 
 			}
 		}
-
-		System.out.println(Arrays.toString(nBucket));
-		System.out.println(Arrays.toString(pBucket));
-
+		
 		insertionSort(nBucket);
 		insertionSort(pBucket);
 		
-		System.out.println("Sorted");
-		System.out.println(Arrays.toString(nBucket));
-		System.out.println(Arrays.toString(pBucket));
-		
-		int index = 0; 
-		
+		int index= 0; 
 		for(int i = nBucket.length-1; i>=0; i--) {
-			for(int j = 0; j<nBucket[i].size(); j++) {
-				arr[index++] =(int)nBucket[i].get(j);
+			for(int j = 0 ; j< nBucket[i].size(); j++) {
+				arr[index++] = (int)nBucket[i].get(j); 
 			}
 		}
-		
-		for(int i = 0; i<pBucket.length; i++) {
+		for(int i = 0; i< pBucket.length; i++) {
 			for (int j = 0; j < pBucket[i].size(); j++) {
 				arr[index++] = (int)pBucket[i].get(j);
 			}
 		}
-		
-		printArray(arr);
 
 	}
 	
-	private static void insertionSort(List [] bucket) {
-		for(List bkt: bucket) {
+	private static void insertionSort(List[] buckets ) {
+		for(List bkt : buckets) {
 			for(int i = 1; i<bkt.size(); i++) {
 				int temp = (int)bkt.get(i);
 				int j = i-1; 
 				while(j>=0 && (int)bkt.get(j)>temp) {
-					if((int)bkt.get(j)>temp) {
+					if((int)bkt.get(j)>(int)bkt.get(j+1)) {
 						bkt.set(j+1, bkt.get(j));
-						j--; 
+						j--;
 					}
-					bkt.set(j+1, temp); 
+					bkt.set(j+1, temp);
 				}
 			}
 		}
+	}
+
+	private static List[] initializeBucket(List[] buckets) {
+		for (int i = 0; i < buckets.length; i++) {
+			buckets[i] = new ArrayList();
+		}
+		return buckets;
 	}
 
 	private static void printArray(int[] arr) {
