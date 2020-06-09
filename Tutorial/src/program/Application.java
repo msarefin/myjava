62659c67bb5d5f5292864504edb46867d062a5df
msarefin <mohammed.arefin@gmail.com>
2020-06-09 12:03:51 -0400

    update

---
+++ Tutorial/src/program/Application.java
@@ -0,0 +1,17 @@
+package program;
+
+public class Application {
+	
+	public static void main (String [] args) {
+		//Main method
+		
+		
+		System.out.println("Hello World!dfsgsfgsdgfsdgqegeg"); 
+		
+		
+	}
+	
+	
+	
+	
+}
