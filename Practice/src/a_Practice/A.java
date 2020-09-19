2120caa89788257aacd5d07897eb0328d9754705
msarefin <mohammed.arefin@gmail.com>
2020-09-19 10:59:47 -0400

    created A.java

--- Practice/src/a_Practice/A.java
+++ Practice/src/a_Practice/A.java
@@ -1,7 +1,31 @@
 package a_Practice;
 
-public class A {
+public class A{
 	public static void main(String[] args) {
-		System.out.println("Hello World@");
+		B b = new B(); 
 	}
+}
+
+class B {
+
+	{
+		System.out.println("This is a non static block");
+	}
+	
+	static {
+		System.out.println("This is a Static block");
+	}
+
+	B(){
+		System.out.println("This is a Constructor!");
+	}
+	
+	void myMethod() {
+		System.out.println("Non static method");
+	}
+	
+	static void myMethod1() {
+		System.out.println("Static Method");
+	}
+	
 }
