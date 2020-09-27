12e2b80d6f0f36dfc834ad052ca7adefdd226418
msarefin <mohammed.arefin@gmail.com>
2020-09-27 19:02:01 -0400

    update

--- Practice/src/a_Practice/OOPConcepts.java
+++ Practice/src/a_Practice/OOPConcepts.java
@@ -1,10 +1,30 @@
 package a_Practice;
 
-public class OOPConcepts {
 
+public class OOPConcepts implements A{
+
+	public static void main(String[] args) {
+		
+		
+	}
 }
 
 //Abstraction
+//	Abstract Class 
+
+class program {
+	 
+}
+
+//	Interface
+
+interface A{
+	
+	program p = new program();
+	
+			
+}
+
 
 
 
