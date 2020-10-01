47453a5d43428d7679f7253a526a346baa633411
msarefin <mohammed.arefin@gmail.com>
2020-10-01 12:16:29 -0400

    Working with garbage collector method and overriding finalize method

--- Practice/src/a_Practice/FinalizeGarbageColector.java
+++ Practice/src/a_Practice/FinalizeGarbageColector.java
@@ -3,17 +3,36 @@ package a_Practice;
 public class FinalizeGarbageColector {
 
 	public static void main(String[] args) {
-		FinalizeGarbageColector g1 = new FinalizeGarbageColector(); 
-		FinalizeGarbageColector g2 = new FinalizeGarbageColector(); 
-		
-		g1 = null; 
-		g2 = null; 
-		
-		System.gc();
+
+		try {
+
+			FinalizeGarbageColector g1 = new FinalizeGarbageColector();
+			FinalizeGarbageColector g2 = new FinalizeGarbageColector();
+
+			g1 = null;
+//			g2 = null;
+
+			g1.justAMethod();
+			g2.justAMethod();
+			
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("The method cannot be called using the referance variable because it is reinitialized as null!");
+
+		} finally {
+
+			System.gc();
+		}
+
 	}
-	
+
+	void justAMethod() {
+		System.out.println("Just a method!");
+	}
+
 	public void finalize() {
 		System.out.println("Called Fianlize method!!!");
 	}
-	
+
 }
