8bad2db19249b9ef71debf1a3400143d190ea4e1
msarefin <mohammed.arefin@gmail.com>
2020-10-08 16:02:10 -0400

    updated String buffer !

--- Practice/src/a_Practice/ImmutableString.java
+++ Practice/src/a_Practice/ImmutableString.java
@@ -1,9 +1,31 @@
 package a_Practice;
 
+import java.awt.Point;
+
 public class ImmutableString {
 
-	String s1 = "hi"; 
-	String s2 = "hi"; 
-	
+	String s1 = "hi";
+	String s2 = "hi";
+
 	int x = 1, y = 1;
+
+	public static void main(String[] args) {
+//		Point myPoint = new Point(0, 0);
+//		System.out.println(myPoint);
+//		myPoint.setLocation(1.0, 0.0);
+//		System.out.println(myPoint);
+		
+//		Immutable object example
+		String myString = new String("Old String"); 
+		System.out.println(myString);
+		myString.replaceAll("Old", "New");
+		System.out.println(myString);
+		
+//		Immutable Object example
+		String s = "How";
+		System.out.println(s); 
+		s.concat(" Are you ?");
+		System.out.println(s);
+		
+	}
 }
