752f5c3427cf3a95ba8302d44dd5cc3f553d2d8d
msarefin <mohammed.arefin@gmail.com>
2020-10-14 18:30:12 -0400

    update

--- Practice/src/a_Practice/SortingAlgorithmPractice.java
+++ Practice/src/a_Practice/SortingAlgorithmPractice.java
@@ -7,8 +7,10 @@ import java.util.Random;
 
 public class SortingAlgorithmPractice {
 
+	static int[] a = new int[] { 5, 6, 4, 7, 3, 8, 2, 9, 1, 0, -9, -1, -8, -2, -7, -3, -6, -4, -5 };
+
 	public static void main(String[] args) {
-		int[] a = new int[] { 5, 6, 4, 7, 3, 8, 2, 9, 1, 0, -9, -1, -8, -2, -7, -3, -6, -4, -5 };
+//		int[] a = null; 
 		sortingMethods s = new sortingMethods();
 		s.BubbleSort(a);
 		s.SelectionSort(a);
@@ -18,8 +20,8 @@ public class SortingAlgorithmPractice {
 
 }
 
-class sortingMethods extends HelperMethods {
-	void BubbleSort(int[] a) {
+class sortingMethods extends HelperMethods implements AlgorithmList {
+	public void BubbleSort(int[] a) {
 		getMethodName();
 		int[] arr = InitializeArray(a);
 		printArray(arr);
@@ -34,7 +36,7 @@ class sortingMethods extends HelperMethods {
 		System.out.println("-".repeat(Arrays.toString(a).length()));
 	}
 
-	void SelectionSort(int[] a) {
+	public void SelectionSort(int[] a) {
 		getMethodName();
 		int[] arr = InitializeArray(a);
 		printArray(arr);
@@ -53,7 +55,7 @@ class sortingMethods extends HelperMethods {
 		System.out.println("-".repeat(Arrays.toString(arr).length()));
 	}
 
-	void InsertionSort(int[] a) {
+	public void InsertionSort(int[] a) {
 		getMethodName();
 		int[] arr = InitializeArray(a);
 		printArray(arr);
@@ -72,7 +74,7 @@ class sortingMethods extends HelperMethods {
 		System.out.println("-".repeat(Arrays.toString(arr).length()));
 	}
 
-	void BucketSort(int[] a) {
+	public void BucketSort(int[] a) {
 		getMethodName();
 		int[] arr = InitializeArray(a);
 		printArray(arr);
@@ -88,86 +90,188 @@ class sortingMethods extends HelperMethods {
 			}
 		}
 
-		int nb = (int) sqrt(neg);
-		int pb = (int) sqrt(pos);
+		List[] nBucket = null;
+		List[] pBucket = null;
 
-		List[] nBucket = new List[nb];
-		List[] pBucket = new List[pb];
+//		Negative Bucket 
+		if (neg > 0) {
+//			Declare buckets
+			int nb = (int) sqrt(pos);
+			nBucket = new List[nb];
 
-		for (int i = 0; i < nBucket.length; i++) {
-			nBucket[i] = new ArrayList();
-		}
-		for (int i = 0; i < pBucket.length; i++) {
-			pBucket[i] = new ArrayList();
-		}
-
-		int min = 0;
-		int max = 0;
-
-		for (int n : arr) {
-			if (min > n) {
-				min = n;
-			} else if (max < n) {
-				max = n;
+//			Initialize the buckets
+			for (int i = 0; i < nBucket.length; i++) {
+				nBucket[i] = new ArrayList();
 			}
-		}
-		
-		for(int n : arr) {
-			int bi = 0; 
-			if(n<0) {
-				bi = (n*nb)/(min-1);
-				nBucket[bi].add(n); 
-			}else { 
-				bi = (n* pb)/(max+1);
-				pBucket[bi].add(n); 
-			}
-		}
-		
-		
-		
-		for(List nbkt: nBucket) {
-			for(int i = 1; i< nbkt.size(); i++) { 
-				int t = (int)nbkt.get(i); 
-				int j = i-1; 
-				while(j >=0 && (int)nbkt.get(j)>t) {
-					nbkt.set(j+1, nbkt.get(j)); 
-					j--; 
+
+//			Determine the min value
+			int min = 0;
+
+			for (int n : arr) {
+				if (min > n) {
+					min = n;
 				}
-				nbkt.set(j+1, t); 
+			}
+
+//			Distribute elements based on range to respective buckets
+			for (int n : arr) {
+				if (n < 0) {
+					int bi = (n * nBucket.length) / (min - 1);
+					nBucket[bi].add(n);
+
+				}
+			}
+
+//			Insertion Sort the Bucket 
+			for (List nbkt : nBucket) {
+				for (int i = 1; i < nbkt.size(); i++) {
+					int t = (int) nbkt.get(i);
+					int j = i - 1;
+					while (j >= 0 && (int) nbkt.get(j) > t) {
+						nbkt.set(j + 1, nbkt.get(j));
+						j--;
+					}
+					nbkt.set(j + 1, t);
+				}
+			}
+
+		}
+
+//		Positive Bucket
+		if (pos > 0) {
+//			Declare buckets
+			int pb = (int) sqrt(pos);
+			pBucket = new List[pb];
+
+//			Initialize Buckets
+			for (int i = 0; i < pBucket.length; i++) {
+				pBucket[i] = new ArrayList();
+			}
+
+//			Determin the max value
+			int max = 0;
+
+			for (int n : arr) {
+				if (max < n) {
+					max = n;
+				}
+			}
+
+//			Distribute elements based on the range to respective buckets
+			for (int n : arr) {
+				if (n >= 0) {
+					int bi = (n * pBucket.length) / (max + 1);
+					pBucket[bi].add(n);
+				}
+			}
+
+			for (List pbkt : pBucket) {
+				for (int i = 1; i < pbkt.size(); i++) {
+					int t = (int) pbkt.get(i);
+					int j = i - 1;
+					while (j >= 0 && (int) pbkt.get(j) > t) {
+						pbkt.set(j + 1, pbkt.get(j));
+						j--;
+					}
+					pbkt.set(j + 1, t);
+				}
 			}
 		}
 		
-		for(List pbkt: pBucket) {
-			for(int i = 1 ; i< pbkt.size(); i++) {
-				int t = (int)pbkt.get(i); 
-				int j = i-1;
-				while(j>=0 && (int)pbkt.get(j)>t) {
-					pbkt.set(j+1, pbkt.get(j));
-					j--; 
+		int index=0; 
+		if(nBucket!=null) {
+			for(int i = nBucket.length-1; i >=0; i--) {
+				for(int j = 0; j< nBucket[i].size(); j++) {
+					arr[index++] = (int)nBucket[i].get(j);
+							
 				}
-				pbkt.set(j+1, t);
+			}
+		}
+		if(pBucket!=null) {
+			for(List pbkt: pBucket) {
+				for(int i = 0; i<pbkt.size(); i++) {
+					arr[index++] = (int)pbkt.get(i); 
+				}
 			}
 		}
 
-//		System.out.println(Arrays.toString(nBucket));
-//		System.out.println(Arrays.toString(pBucket));
-		
-		int index =0;
-		for(int i = nBucket.length-1; i>=0; i--) {
-			for(int j = 0; j< nBucket[i].size(); j++) {
-				arr[index++] = (int)nBucket[i].get(j); 
-			}
-		}
-		
-		for(int i = 0; i< pBucket.length; i++) {
-			for(int j = 0; j< pBucket[i].size(); j++) {
-				arr[index++] =(int)pBucket[i].get(j); 
-			}
-		}
-		
+		System.out.println(Arrays.toString(nBucket));
+		System.out.println(Arrays.toString(pBucket));
+
 		printArray(arr);
 		System.out.println("-".repeat(Arrays.toString(arr).length()));
 	}
+
+	@Override
+	public void MergeSort(int[] a) {
+		// TODO Auto-generated method stub
+		getMethodName();
+		int [] arr = InitializeArray(a); 
+		printArray(arr);
+		mSort(arr);
+		printArray(arr);
+		System.out.println("-".repeat(Arrays.toString(arr).length()));
+
+	}
+	
+	private static void mSort(int [] arr) {
+		if(arr==null) {
+			return;
+		}
+		if(arr.length>1) {
+			int mid = arr.length/2;
+			
+			int[] left = new int [mid];
+			int[] right = new int [arr.length-mid]; 
+			
+			for(int i =0; i< arr.length; i++) {
+				if(i< mid) {
+					left[i] = arr[i]; 
+				}else {
+					right[i-mid] = arr[i];
+				}
+			}
+			mSort(left);
+			mSort(right);
+		}
+	}
+
+	@Override
+	public void QuickSort(int[] a) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void HeapSort(int[] a) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void ShellSort(int[] a) {
+		// TODO Auto-generated method stub
+
+	}
+}
+
+interface AlgorithmList {
+	void BubbleSort(int[] a);
+
+	void SelectionSort(int[] a);
+
+	void InsertionSort(int[] a);
+
+	void BucketSort(int[] a);
+
+	void MergeSort(int[] a);
+
+	void QuickSort(int[] a);
+
+	void HeapSort(int[] a);
+
+	void ShellSort(int[] a);
+
 }
 
 class HelperMethods {
