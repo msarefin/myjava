00dde75d0f7f6784b0cf3c50d5500222fd2e79d9
msarefin <mohammed.arefin@gmail.com>
2020-11-13 12:30:43 -0500

    update

--- Practice/src/a_Practice/JustPractice.java
+++ Practice/src/a_Practice/JustPractice.java
@@ -2,45 +2,37 @@ package a_Practice;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
 
 public class JustPractice {
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) {
 
-		String pathname = "C:/Users/sunsh/Documents/JavaProject/EclipseProject/myjava/Practice/Files/Topics.txt";
-		
-		new externalDataSource().readFileUsingScanner(pathname);
-		
-		
-	}
+		try {
+			Path path = Paths.get(System.getProperty("user.dir")+"/Created Files/");
+			Files.createDirectories(path);
+			FileWriter fw = new FileWriter(System.getProperty("user.dir") + "/Created Files/Written.txt");
+			PrintWriter pw = new PrintWriter(fw);
+			pw.print("Text written using PrintWriter\n");
+			pw.printf("%s cost %d", "ALM Software", 25000);
 
-	static class externalDataSource{
-		void readFileUsingScanner(String pathname) {
-			File file = new File(pathname);
-			try {
-				Scanner sc = new Scanner(file);
-				while(sc.hasNext()) { 
-					System.out.println(sc.nextLine());
-				}
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
+			pw.close();
+			fw.close();
 			
-			System.out.println("\n The lines are divided into a List of array \n");
 			
-			try {
-				List<String> stream = Files.readAllLines(Paths.get(pathname));
-				System.out.println(stream);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+
 	}
+	
+	
 }
