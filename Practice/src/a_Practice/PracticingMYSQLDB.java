c64f84592a230e1bc6e167a62ea8af43aa7f9f0e
msarefin <mohammed.arefin@gmail.com>
2020-11-25 13:22:43 -0500

    update

--- Practice/src/a_Practice/PracticingMYSQLDB.java
+++ Practice/src/a_Practice/PracticingMYSQLDB.java
@@ -11,26 +11,21 @@ import java.sql.Statement;
 
 public class PracticingMYSQLDB {
 
-	private static String jdbcDriver = "com.mysql.cj.jdbc.Driver";
-	private static String url="jdbc:mysql://localhost:3306/pnt?userTimezone=true&serverTimezone=GMT%2B8";
-	private static String user = "msarefin";
-	private static String password = "abc123";
 	
-	static String sql = null; 
+	
+}
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		Connection con = DriverManager.getConnection(url, user, password); 
-		Statement statement = con.createStatement(); 
-		sql = "select * from student"; 
-		ResultSet rs = statement.executeQuery(sql); 
-		
-		while(rs.next()) { 
-			int id = rs.getInt("id"); 
-			String name = rs.getString("student_name"); 
-			String dob = rs.getString("date_of_birth"); 
-			System.out.println(id+"-->"+name+"-->"+dob);
+
+class MySQLAccess{ 
+	private Connection  con = null; 
+	private Statement st = null;
+	private PreparedStatement ps = null; 
+	private ResultSet rs = null; 
+	
+	public static void readFromDB() {
+		try {
+			
 		}
-		con.close();
-		
 	}
+	
 }
