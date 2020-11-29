7c1d9fb66738b99cd30ef64a3e359e0f0f7b9f43
msarefin <mohammed.arefin@gmail.com>
2020-11-28 19:31:42 -0500

    moves the Topics file

--- Practice/src/a_Practice/PracticingMYSQLDB.java
+++ Practice/src/a_Practice/PracticingMYSQLDB.java
@@ -7,25 +7,28 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
-
 public class PracticingMYSQLDB {
 
-	
-	
 }
 
+class MySQLAccess {
+	private static Connection con = null;
+	private static Statement st = null;
+	private static PreparedStatement ps = null;
+	private static ResultSet rs = null;
 
-class MySQLAccess{ 
-	private Connection  con = null; 
-	private Statement st = null;
-	private PreparedStatement ps = null; 
-	private ResultSet rs = null; 
-	
 	public static void readFromDB() {
 		try {
-			
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 	}
-	
+
 }
