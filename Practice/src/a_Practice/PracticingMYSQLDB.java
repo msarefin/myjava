3d5a882c2fc03d395d26967c450abd59a3725557
msarefin <mohammed.arefin@gmail.com>
2020-12-02 15:27:01 -0500

    update

--- Practice/src/a_Practice/PracticingMYSQLDB.java
+++ Practice/src/a_Practice/PracticingMYSQLDB.java
@@ -10,27 +10,59 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 import javax.swing.InputMap;
 
+import com.mysql.cj.jdbc.Driver;
+import com.mysql.cj.protocol.Resultset;
+
 public class PracticingMYSQLDB {
 
-}
+	static String driver, url, userName, password;
 
-class MySQLAccess {
-	private static Connection con = null;
-	private static Statement st = null;
-	private static PreparedStatement ps = null;
-	private static ResultSet rs = null;
+	public static void main(String[] args) throws Exception {
 
-	private static Properties loadProperties() throws IOException { 
+		InputStream input = new FileInputStream(System.getProperty("user.dir") + "/Files/mysql.properties");
 		Properties prop = new Properties();
-		InputStream ism = new FileInputStream(System.getProperty("user.dir")+"/Files/mysql.properties"); 
-		prop.load(ism);
-		ism.close();
-		return prop; 
-		
-	}
+		prop.load(input);
 
+		driver = prop.getProperty("MYSQLJDBC.Driver");
+		url = prop.getProperty("MYSQLJDBC.url");
+		userName = prop.getProperty("MYSQLJDBC.userName");
+		password = prop.getProperty("MYSQLJDBC.password");
+
+		Class.forName(driver);
+		Connection con = DriverManager.getConnection(url, userName, password);
+		Statement stmt = con.createStatement();
+		String sqlInsert = "insert into Students(name, age) values(\"Mohammed\", 38)";
+		String sqlUpdate = "update Students set age = 21 where id = 3";
+		String sqlDelete = "delete from students where id>6";
+		String sql = "select * from Students";
+		int rowsEffected = stmt.executeUpdate(sqlInsert);
+		rowsEffected = stmt.executeUpdate(sqlUpdate);
+		rowsEffected = stmt.executeUpdate(sqlDelete);
+		System.out.println("Number of rows affected " + rowsEffected);
+		ResultSet re = stmt.executeQuery(sql);
+
+		List<ArrayList> result = new ArrayList<ArrayList>();
+
+		while (re.next()) {
+			ArrayList rows = new ArrayList();
+			rows.add(re.getInt("ID"));
+			rows.add(re.getString("name"));
+			rows.add(re.getInt("age"));
+			result.add(rows);
+
+		}
+		input.close();
+
+		for (int i = 0; i < result.size(); i++) {
+			System.out.println((result.get(i).toString()));
+		}
+
+	}
 }
