48b856c74f2b4d9043e4fede0c941af8f05728ee
msarefin <mohammed.arefin@gmail.com>
2020-12-11 21:31:02 -0500

    Practicing Google drive and Google Sheet need to creat a new file

--- Practice/src/a_Practice/PracticeGooleSheet.java
+++ Practice/src/a_Practice/PracticeGooleSheet.java
@@ -18,8 +18,12 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.FileList;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.UpdateValuesResponse;
+import com.google.api.services.sheets.v4.model.ValueRange;
 import com.mysql.cj.jdbc.jmx.LoadBalanceConnectionGroupManager;
 
 public class PracticeGooleSheet {
@@ -27,6 +31,8 @@ public class PracticeGooleSheet {
 	static JsonFactory  jsonFactory = JacksonFactory.getDefaultInstance(); 
 	
 	public static void main(String[] args) throws Exception {
+//		Connect to Google API
+		
 		JsonFactory  jsonFactory = JacksonFactory.getDefaultInstance();
 		File file = new File(System.getProperty("user.dir")+"/Files/resources/client_secret.json");
 		FileDataStoreFactory dataStroeFactory = new FileDataStoreFactory(file); 
@@ -34,16 +40,31 @@ public class PracticeGooleSheet {
 		InputStream inputStream = PracticeGooleSheet.class.getResourceAsStream("client_secret.json");
 		InputStreamReader credencialLocation = new InputStreamReader(inputStream); 
 		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(jsonFactory, credencialLocation); 
-		List<String> scope = Arrays.asList(SheetsScopes.SPREADSHEETS);
+		List<String> scopes = Arrays.asList(SheetsScopes.SPREADSHEETS);
 		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(httpTransport, jsonFactory, clientSecrets, scopes).setDataStoreFactory(dataStroeFactory).setAccessType("offline").build();
 		LocalServerReceiver receiver = new LocalServerReceiver(); 
 		Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
 		
-		String appname= "MyGoogleSheet"; 
-		Sheets sheets = new Sheets.Builder(httpTransport, jsonFactory, credential).setApplicationName(appname).build(); 
+//		Obtain Drive service
 		
 		
+		
+//		Obtain sheet service
+		
+//		String appname= "MyGoogleSheet"; 
+//		
+//		Sheets sheets = new Sheets.Builder(httpTransport, jsonFactory, credential).setApplicationName(appname).build(); 
+//		
+////		Single range write 
+//		
+//		String spreadsheetId =""; 
+//		String range = "A1"; 
+//		
+//		ValueRange content = new ValueRange().setValues(Arrays.asList(Arrays.asList("Expense January"), Arrays.asList("books","30"),Arrays.asList("pens","10"), Arrays.asList("Expense February"),Arrays.asList("cloths","30"),Arrays.asList("shoes","5"))); 
+//		UpdateValuesResponse result = sheets.spreadsheets().values().update(spreadsheetId, range, content).setValueInputOption("RAW").execute(); 
 		
 	}
 
 }
+
+
