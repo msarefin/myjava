392e64210451a6c95c594ff624cc19aad8e5240e
msarefin <mohammed.arefin@gmail.com>
2020-12-30 01:54:00 -0500

    working with selenium webdrver

--- Tutorial/src/webAutomation/practiceWebAutomation.java
+++ Tutorial/src/webAutomation/practiceWebAutomation.java
@@ -1,28 +1,42 @@
 package webAutomation;
 
+import java.sql.Driver;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class practiceWebAutomation {
 
+	static String url = "https://www.amazon.com";
+	
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/drivers/geckodriver.exe");
+
+		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/drivers/geckodriver.exe");
+
 		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.amazon.com/");
-		String title = driver.getTitle(); 
-		System.out.println(title);
-		driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
-		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("ipad pro");
-		driver.findElement(By.id("twotabsearchtextbox")).sendKeys(Keys.ENTER);
-		Thread.sleep(5000);
+		driver.get(url);
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		title = driver.getTitle();
+		String title = driver.getTitle();
 		System.out.println(title);
+		
+		WebElement searchBar = driver.findElement(By.cssSelector("input#twotabsearchtextbox.nav-input"));
+		WebElement searchButton = driver.findElement(By.cssSelector("#nav-search-submit-text > input"));
+	
+		searchBar.clear();
+		searchBar.click();
+		searchBar.sendKeys("ipad air");
+		searchButton.click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		title = driver.getTitle(); 
+		System.out.println(title);
+//		Thread.sleep(5000);
 		driver.close();
+
 	}
 }
--- Tutorial/src/webAutomation/practicingWithChromeDriver.java
+++ Tutorial/src/webAutomation/practicingWithChromeDriver.java
@@ -8,11 +8,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class practicingWithChromeDriver {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/drivers/");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/drivers/chromedriver.exe");
 	
 		WebDriver driver = new ChromeDriver(); 
 		driver.manage().window().maximize();
-		driver.get("http://www.yahoo.com");
+		driver.get("http://www.demoqa.com/login");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 
 		String title= driver.getTitle(); 
 		System.out.println(title);
