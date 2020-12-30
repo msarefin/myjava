7c0b1eb4cb377d01fa139dca89a0c7c4a57cf403
msarefin <mohammed.arefin@gmail.com>
2020-12-30 03:11:41 -0500

    update

--- Tutorial/src/webAutomation/practiceWebAutomation.java
+++ Tutorial/src/webAutomation/practiceWebAutomation.java
@@ -15,32 +15,38 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class practiceWebAutomation {
 
 	static String url = "https://www.amazon.com";
-	
+
 	public static void main(String[] args) throws InterruptedException {
 
 		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/drivers/geckodriver.exe");
-		DesiredCapabilities capabilities = DesiredCapabilities.firefox(); 
+		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
 		capabilities.setCapability("marionette", true);
-		
+
 		FirefoxOptions options = new FirefoxOptions();
-		options.setHeadless(true); 
-		WebDriver driver = new FirefoxDriver(options);
+		options.setHeadless(true);
+		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.get(url);
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		String title = driver.getTitle();
 		System.out.println(title);
-		
+
 		WebElement searchBar = driver.findElement(By.cssSelector("input#twotabsearchtextbox.nav-input"));
 		WebElement searchButton = driver.findElement(By.cssSelector("#nav-search-submit-text > input"));
-	
+
 		searchBar.clear();
 		searchBar.click();
 		searchBar.sendKeys("ipad air");
 		searchButton.click();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		title = driver.getTitle(); 
+		title = driver.getTitle();
 		System.out.println(title);
+		String currentURL = driver.getCurrentUrl(); 
+		System.out.println(currentURL);
+		String pageDOM = driver.getPageSource();
+		System.out.println(pageDOM);
+		
+		
 //		Thread.sleep(5000);
 		driver.close();
 
