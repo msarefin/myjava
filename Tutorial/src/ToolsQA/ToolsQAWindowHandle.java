11673daf109621fed731a2a38324ee791ff6916e
msarefin <mohammed.arefin@gmail.com>
2021-01-05 19:41:06 -0500

    window handel

--- Tutorial/src/ToolsQA/ToolsQAWindowHandle.java
+++ Tutorial/src/ToolsQA/ToolsQAWindowHandle.java
@@ -1,5 +1,18 @@
 package ToolsQA;
 
-public class ToolsQAWindowHandle {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class ToolsQAWindowHandle {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver(); 
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		
+		
+		
+		driver.close();
+		driver.quit();
+	}
 }
