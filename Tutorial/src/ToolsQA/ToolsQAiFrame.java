584e1c28c8b99aa818a683b480d5cfe299462395
msarefin <mohammed.arefin@gmail.com>
2021-01-08 00:23:30 -0500

    Learned iframe

--- Tutorial/src/ToolsQA/ToolsQAiFrame.java
+++ Tutorial/src/ToolsQA/ToolsQAiFrame.java
@@ -1,8 +1,37 @@
 package ToolsQA;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class ToolsQAiFrame {
 
 	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		String parentWindow = driver.getWindowHandle();
+		System.out.println(parentWindow);
+		driver.get("https://demoqa.com/frames");
 		
+		WebDriverWait wait = new WebDriverWait(driver, 10); 
+		
+		wait.until(ExpectedConditions.visibilityOfAllElements(driver.findElements(By.tagName("iframe"))));
+		
+		driver.switchTo().frame(0); 
+		System.out.println(driver.getWindowHandle());
+		System.out.println(driver.getTitle());
+		driver.switchTo().defaultContent();
+		System.out.println(driver.getWindowHandle());
+		driver.switchTo().frame(1); 
+		System.out.println(driver.getWindowHandle());
+		System.out.println(driver.getTitle());
+		driver.switchTo().defaultContent();
+		System.out.println(driver.getCurrentUrl());
+		
+		driver.quit();
 	}
 }
