4068936b51e388c105bfacae0b46762b0889bc51
msarefin <mohammed.arefin@gmail.com>
2021-01-14 23:37:08 -0500

    still working with headless browser

--- Tutorial/src/ToolsQA/ToolsQAHeadless.java
+++ Tutorial/src/ToolsQA/ToolsQAHeadless.java
@@ -2,4 +2,7 @@ package ToolsQA;
 
 public class ToolsQAHeadless {
 
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/drivers/chromedriver.exe");
+	}
 }
