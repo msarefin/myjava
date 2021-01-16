53c192d28c7c9cb95d6d8a7f23ae6968f832f4ef
msarefin <mohammed.arefin@gmail.com>
2021-01-15 19:05:11 -0500

    update

--- Tutorial/src/ToolsQA/ToolsQAHeadless.java
+++ Tutorial/src/ToolsQA/ToolsQAHeadless.java
@@ -8,17 +8,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ToolsQAHeadless {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/drivers/chromedriver.exe");
-		WebDriver driver = new ChromeDriver(); 
 		
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); 
-		driver.manage().window().maximize();
-		String mainWindow = driver.getWindowHandle();
-		
-		driver.get("");
-		
-		driver.close();
-		driver.quit();
 	}
 }
