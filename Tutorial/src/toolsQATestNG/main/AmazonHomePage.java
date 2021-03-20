02c482a3b4b4a4eccd432d1dc8ca9195f6b555ef
msarefin <mohammed.arefin@gmail.com>
2021-03-19 21:32:29 -0400

    update

--- Tutorial/src/toolsQATestNG/main/AmazonHomePage.java
+++ Tutorial/src/toolsQATestNG/main/AmazonHomePage.java
@@ -10,8 +10,16 @@ import org.testng.annotations.Test;
 import toolsQATestNG.Generic.Base.CommonAPI;
 
 public class AmazonHomePage extends CommonAPI {
+	
+	 public WebElement department = singleElement("cssSelector", "select.nav-search-dropdown");
+	    public List<WebElement> departmentList = ListOfWebElements("cssSelector", "select.nav-search-dropdown>option");
+	    public WebElement searchTextArea = singleElement("cssSelector", "input#twotabsearchtextbox.nav-input.nav-progressive-attribute");
+	    public WebElement searchButton = singleElement("cssSelector", "input#nav-search-submit-button.nav-input.nav-progressive-attribute");
+
 
 	public static void getListOfAllDepartments() {
+		
+		
 		WebElement element = findELementBy("cssSelector", "select#searchDropdownBox");
 		Select select = new Select(element);
 
