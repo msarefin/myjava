52f43f2ed4bfb9c6806fa727eebf710d8d137858
msarefin <mohammed.arefin@gmail.com>
2021-07-31 06:58:55 -0400

    update

--- Tutorial/src/newPractice/Practice.java
+++ Tutorial/src/newPractice/Practice.java
@@ -1,7 +1,18 @@
 package newPractice;
 
 public class Practice {
-	public static void main(String[] args) {
-		System.out.println("This is my Pactice session");
+	
+	void hello() {
+		System.out.println("Hello, Everyone!!!");
 	}
+	
+	void greetings() {
+		this.hello();
+	}
+	
+	public static void main(String[] args) {
+		new Practice().greetings();
+		
+	}
+	
 }
