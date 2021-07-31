77589e6bc68a92d103e7e203d759eec348879330
msarefin <mohammed.arefin@gmail.com>
2021-07-31 11:44:27 -0400

    update

--- Tutorial/src/newPractice/Practice.java
+++ Tutorial/src/newPractice/Practice.java
@@ -1,18 +1,26 @@
 package newPractice;
 
 public class Practice {
-	
+
 	void hello() {
 		System.out.println("Hello, Everyone!!!");
 	}
-	
+
 	void greetings() {
 		this.hello();
 	}
-	
+
 	public static void main(String[] args) {
 		new Practice().greetings();
-		
+
 	}
-	
+
+}
+
+class parent {
+	String gender(String g) {
+		String msg = "";
+		
+		if(msg.equals(msg))
+	}
 }
