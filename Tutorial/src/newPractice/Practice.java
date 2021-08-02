1d42353843e0ba2e5a188558af7d75f74975c137
msarefin <mohammed.arefin@gmail.com>
2021-08-02 11:11:33 -0400

    this is my practice !

--- Tutorial/src/newPractice/Practice.java
+++ Tutorial/src/newPractice/Practice.java
@@ -2,7 +2,7 @@ package newPractice;
 
 public class Practice extends parent {
 	
-	String n = super.gender("sagk"); 
+	String n = super.gender("man"); 
 
 	void hello() {
 		System.out.println("Hello, Everyone!!!");
@@ -19,7 +19,15 @@ public class Practice extends parent {
 
 }
 
-class parent {
+class parent implements human{
+	
+	    parent(){
+		eyes("hazel");
+		ears();
+		nose("pointy");
+		System.out.println(name("Adam"));
+	}
+	
 	String gender(String g) {
 		String msg = "";
 		
@@ -33,4 +41,35 @@ class parent {
 		
 		return msg; 
 	}
+
+	@Override
+	public void eyes(String n ) {
+		// TODO Auto-generated method stub
+		System.out.println("My eyes are "+n);
+	}
+
+	@Override
+	public void ears() {
+		// TODO Auto-generated method stub
+		System.out.println("My ears are small!");
+	}
+
+	@Override
+	public void nose(String n) {
+		// TODO Auto-generated method stub
+		System.out.println("My nose is "+ n );
+	}
+
+	@Override
+	public String name(String name) {
+		// TODO Auto-generated method stub
+		return "My name is "+name;
+	}
+}
+
+interface human{
+	void eyes(String eyes);
+	void ears(); 
+	void nose(String nose); 
+	String name(String name);
 }
