85cf7d4f5ba8a96ffd210108aa80dcfcf6193cac
msarefin <mohammed.arefin@gmail.com>
2021-08-02 10:57:23 -0400

    update

--- Tutorial/src/newPractice/Practice.java
+++ Tutorial/src/newPractice/Practice.java
@@ -1,6 +1,8 @@
 package newPractice;
 
-public class Practice {
+public class Practice extends parent {
+	
+	String n = super.gender("sagk"); 
 
 	void hello() {
 		System.out.println("Hello, Everyone!!!");
@@ -12,7 +14,7 @@ public class Practice {
 
 	public static void main(String[] args) {
 		new Practice().greetings();
-
+		System.out.println(new Practice().n);
 	}
 
 }
@@ -21,6 +23,14 @@ class parent {
 	String gender(String g) {
 		String msg = "";
 		
-		if(msg.equals(msg))
+		if(g.equals("female") || g.equals("woman")) {
+			msg =  "I am a Mother";
+		}else if (g.equals("male")|| g.equals("man")) {
+			msg =  "I am a Father";
+		}else {
+		msg = "Choose either between man and woman or male and female";
+		}
+		
+		return msg; 
 	}
 }
