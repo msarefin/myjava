fc0e22f98bb09ecbb423a883c632756b26e82aef
Sunny <mohammed.arefin@gmail.com>
2021-08-21 07:48:41 -0400

    updating

--- TomcatDynamicWebProjectJavServlet/src/main/java/com/tomcat/AddServlet.java
+++ TomcatDynamicWebProjectJavServlet/src/main/java/com/tomcat/AddServlet.java
@@ -9,6 +9,9 @@ import jakarta.servlet.http.HttpServletResponse;
 public class AddServlet extends HttpServlet{
 
 	public void service(HttpServletRequest req, HttpServletResponse res) {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
 		
+		int sum = i+j ; 
 	}
 }
