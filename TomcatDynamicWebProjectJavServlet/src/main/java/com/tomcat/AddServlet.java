57605d539448550448d317f009528e16d595b855
Sunny <sunny@Sunnys-MacBook-Pro.local>
2021-08-21 11:26:57 -0400

    successfully created java servlet web application

--- TomcatDynamicWebProjectJavServlet/src/main/java/com/tomcat/AddServlet.java
+++ TomcatDynamicWebProjectJavServlet/src/main/java/com/tomcat/AddServlet.java
@@ -8,10 +8,13 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet{
 
-	public void service(HttpServletRequest req, HttpServletResponse res) {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int sum = i+j ; 
+		
+		PrintWriter out = res.getWriter(); 
+		out.println("Your Sum is :"+sum);
 	}
 }
