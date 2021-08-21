98f8a8ee55247e14e10464abc50fc5995cf71b8c
Sunny <sunny@Sunnys-MacBook-Pro.local>
2021-08-21 07:04:04 -0400

    added jakarta.servlet.http.* in to the project pwd

--- TomcatDynamicWebProjectJavServlet/src/main/java/com/tomcat/AddServlet.java
+++ TomcatDynamicWebProjectJavServlet/src/main/java/com/tomcat/AddServlet.java
@@ -1,6 +1,14 @@
 package com.tomcat;
 
-public class AddServlet extends HttpServerImpl{
+import java.io.*;
 
-	
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet{
+
+	public void service(HttpServletRequest req, HttpServletResponse res) {
+		
+	}
 }
