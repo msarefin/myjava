9969b079c04e33fe036bf474689f21390b7b7b38
msarefin <mohammed.arefin@gmail.com>
2021-08-23 00:38:52 -0400

    html methods get and post

--- TomcatDynamicWebProjectJavServlet/src/main/java/com/tomcat/AddServlet.java
+++ TomcatDynamicWebProjectJavServlet/src/main/java/com/tomcat/AddServlet.java
@@ -6,15 +6,35 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class AddServlet extends HttpServlet{
+public class AddServlet extends HttpServlet {
 
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//		int i = Integer.parseInt(req.getParameter("num1"));
+//		int j = Integer.parseInt(req.getParameter("num2"));
+//		
+//		int sum = i+j ; 
+//		
+//		PrintWriter out = res.getWriter(); 
+//		out.println("Your Sum is :"+sum);
+//	}
+
+//	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//		int i = Integer.parseInt(req.getParameter("num1"));
+//		int j = Integer.parseInt(req.getParameter("num2"));
+//
+//		int sum = i + j;
+//
+//		PrintWriter out = res.getWriter();
+//		out.println("Your Sum is :" + sum);
+//	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
-		
-		int sum = i+j ; 
-		
-		PrintWriter out = res.getWriter(); 
-		out.println("Your Sum is :"+sum);
+
+		int sum = i + j;
+
+		PrintWriter out = res.getWriter();
+		out.println("Your Sum is :" + sum);
 	}
 }
