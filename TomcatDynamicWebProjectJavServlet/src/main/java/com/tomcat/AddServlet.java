56f6cc76d7d05c6f2b73ffa7c457f322472810f4
msarefin <mohammed.arefin@gmail.com>
2021-08-24 01:36:28 -0400

    calling servlet in servlet

--- TomcatDynamicWebProjectJavServlet/src/main/java/com/tomcat/AddServlet.java
+++ TomcatDynamicWebProjectJavServlet/src/main/java/com/tomcat/AddServlet.java
@@ -2,9 +2,13 @@ package com.tomcat;
 
 import java.io.*;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+
+
 
 public class AddServlet extends HttpServlet {
 
@@ -18,7 +22,19 @@ public class AddServlet extends HttpServlet {
 //		out.println("Your Sum is :"+sum);
 //	}
 
-//	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+
+		int sum = i + j;
+
+		PrintWriter out = res.getWriter();
+		out.println("Your Sum is :" + sum);
+		
+		RequestDispatcher
+	}
+
+//	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
 //		int i = Integer.parseInt(req.getParameter("num1"));
 //		int j = Integer.parseInt(req.getParameter("num2"));
 //
@@ -27,14 +43,4 @@ public class AddServlet extends HttpServlet {
 //		PrintWriter out = res.getWriter();
 //		out.println("Your Sum is :" + sum);
 //	}
-
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
-		int i = Integer.parseInt(req.getParameter("num1"));
-		int j = Integer.parseInt(req.getParameter("num2"));
-
-		int sum = i + j;
-
-		PrintWriter out = res.getWriter();
-		out.println("Your Sum is :" + sum);
-	}
 }
---
+++ TomcatDynamicWebProjectJavServlet/src/main/java/com/tomcat/SqServlet.java
@@ -0,0 +1,15 @@
+package com.tomcat;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SqServlet extends HttpServlet{
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		PrintWriter out = res.getWriter(); 
+		out.println("This is a sq Result");
+	}
+}
