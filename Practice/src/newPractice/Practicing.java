474d813030d5bfa0ee96e0b9a53c992f1e11fe5a
msarefin <mohammed.arefin@gmail.com>
2021-10-04 23:26:24 -0400

    Still learning now

--- Practice/src/newPractice/Practicing.java
+++ Practice/src/newPractice/Practicing.java
@@ -2,16 +2,32 @@ package newPractice;
 
 public class Practicing {
 	public static void main(String[] args) {
-		 
+		single_ton s = single_ton.getConstructor(); 
 	}
-	
+
 	private void a() {
 		System.out.println("This is A");
 	}
-	
+
 	private void b() {
 		this.a();
 		System.out.println("This is B");
 	}
 }
 
+class single_ton {
+	static single_ton single ;
+
+	private single_ton() {
+		System.out.println("Singleton class");
+	}
+	
+	public static single_ton getConstructor() {
+		if(single == null ) {
+			single = new single_ton(); 
+		}
+		
+		return single;
+		
+	}
+}
